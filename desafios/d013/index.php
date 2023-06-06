47149b119462707cd8984b201568cd339da19b11
Tiago Oliveira <tiago.oliveira06@gmail.com>
2023-06-06 15:01:18 -0300

    Update index.php

--- desafios/d013/index.php
+++ desafios/d013/index.php
@@ -6,40 +6,49 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Desafio PHP</title>
     <link rel="stylesheet" href="../../style.css">
+    <style>
+        img.nota{
+            height: 50px;
+            margin: 5px;
+        }
+    </style>
 </head>
 <body>
     <?php 
-        $total = $_GET['valor'] ?? 0;
+        $saque = $_REQUEST['valor'] ?? 0;
     ?>
     <main>
         <h1>Caixa Eletrônico</h1>
         <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
-            <label for="valor">Qual valor você deseja sacar? (R$)*</label>
-            <input type="number" name="valor" id="valor" min="5" step="5" value="<?=$total?>" required>
-            <small>*Notas disponíveis: R$ 100,R$ 50, R$10 e R$5</small>
+            <label for="valor">Qual valor você deseja sacar? (R$)<sup>*</sup></label>
+            <input type="number" name="valor" id="valor" min="5" step="5" value="<?=$saque?>" required>
+            <p style="font-size: 0.6em"><sup>*</sup>Notas disponíveis: R$ 100,R$ 50, R$10 e R$5</p>
             <input type="submit" value="Sacar">
         </form>
     </main>
     <?php
-        $n100 = (int)($total / 100);
-        $sobra = ($total % 100);
-
-        $n50 = (int)($sobra / 50);
-        $sobra = ($sobra % 50);
-
-        $n10 = (int)($sobra / 10);
-        $sobra = ($sobra % 10);
-
-        $n5 = (int)($sobra / 5);
+        $resto = $saque;
+        // Saque de R$100
+        $tot100 = floor($resto / 100);
+        $resto = $resto % 100;
+        // Saque de R$50
+        $tot50 = floor($resto / 50);
+        $resto  %= 50;
+        // Saque de R$10
+        $tot10 = floor($resto / 10);
+        $resto %= 10;
+        // Saque de R$5
+        $tot5 = floor($resto / 5);
+        $resto %= 5;
     ?>
     <section>
-        <h2>Saque de R$ <?=number_format($total, 0, ",", ".")?> realizado</h2>
+        <h2>Saque de R$ <?=number_format($saque, 0, ",", ".")?> realizado</h2>
         <p>O caixa eletrônico vai te entregar as seguintes notas:</p>
         <ul>
-            <li><img src="100-reais.jpg" height="100" alt="R$ 100"> x<?=$n100?></li>
-            <li><img src="50-reais.jpg" height="100" alt="R$ 100">  x<?=$n50?></li>
-            <li><img src="10-reais.jpg" height="100" alt="R$ 100">  x<?=$n10?></li>
-            <li><img src="5-reais.jpg" height="100" alt="R$ 100">  x<?=$n5?></li>
+            <li><img src="imagens/100-reais.jpg" class="nota" alt="Nota de 100"> x<?=$tot100?></li>
+            <li><img src="imagens/50-reais.jpg" class="nota" alt="Nota de 50">  x<?=$tot50?></li>
+            <li><img src="imagens/10-reais.jpg" class="nota" alt="Nota de 10">  x<?=$tot10?></li>
+            <li><img src="imagens/5-reais.jpg" class="nota" alt="Nota de 5">  x<?=$tot5?></li>
         </ul>
     </section>
 </body>
